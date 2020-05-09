87d9ed30d06e6ecff382c91ca040550060888656
Zoe Xanos <zxanos@gmail.com>
2020-05-09 02:08:18 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,11 +2,7 @@ require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    for i in 0..10
-      i = 's'
-      i + string
-      puts string
-    end
+    's' * 10
   else
     string
   end
